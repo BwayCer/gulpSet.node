70f7a34a64c1d7cc8c5f872e780391c7e9171818
BwayCer <bwaycer@gmail.com>
2023-06-23 13:20:11 +0800

    refactor: 重新調整功能分布

--- src/index.js
+++
@@ -1,9 +0,0 @@
-
-export {default as gulpRun} from './run.js';
-export {default as GulpTool} from './GulpTool.js';
-export * from './utils.js';
-
-import {gulpPug, gulpPugScript} from './plugin/pug.js';
-import {gulpSass} from './plugin/sass.js';
-export let plugin = {gulpPug, gulpPugScript, gulpSass};
-
