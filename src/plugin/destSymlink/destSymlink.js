71e71a6ab6849798dc00a6a71a1be939281d8565
BwayCer <bwaycer@gmail.com>
2023-07-05 08:51:26 +0800

    feat(plugin-destSymlink): modify 提供強制覆蓋文件選項

--- src/plugin/destSymlink/destSymlink.js
+++ src/plugin/destSymlink/destSymlink.js
@@ -17,20 +17,21 @@ import {fsMkdir, fsSymlink} from '../../utils/fsQuick.js';
 // NOTE:
 // 1. gulp.symlink 會遇到 Error: premature close 問題
 // 2. 此方法不會為非鏈結文件的資料夾建立鏈結文件。
-export default function gulpDestSymlink(directory) {
+export default function gulpDestSymlink(directory, options) {
+  let isForce = (options?.force ?? false) === true;
   let dirList = [];
 
   return new Transform({
     async transform(chunk, encoding, callback) {
-      let srcFileStat = await fsPromises.stat(chunk.path);
+      let {cwd, base, path: filePath} = chunk;
+      let srcFileStat = await fsPromises.stat(filePath);
       if (srcFileStat.isDirectory()) {
-        dirList.push({
-          cwd: chunk.cwd,
-          base: chunk.base,
-          path: chunk.path,
-        });
+        dirList.push({cwd, base, path: filePath});
       } else {
-        await createSymlink(directory, chunk.cwd, chunk.base, chunk.path);
+        let linkPath = _resolveLinkPath(directory, cwd, base, filePath);
+        if (isForce || !fs.existsSync(linkPath)) {
+          await createSymlink(linkPath, filePath);
+        }
       }
       callback(null);
     },
@@ -46,17 +47,14 @@ export default function gulpDestSymlink(directory) {
       // 2. 剩餘的目錄路徑，再過濾掉建立目的地已存在文件的路徑。(避免覆蓋已處理好的文件)
       // 3. 剩餘的目錄路徑，挑出原目錄為鏈結文件的建立鏈結文件。
       for (let idx = 0, len = dirList.length; idx < len; idx++) {
-        let item = dirList[idx];
-        let linkPath = path.join(
-          path.resolve(item.cwd, directory),
-          path.relative(item.base, item.path),
-        );
+        let {cwd, base, path: filePath} = dirList[idx];
+        let linkPath = _resolveLinkPath(directory, cwd, base, filePath);
         if (fs.existsSync(linkPath)) {
           continue;
         }
-        let srcFileLstat = await fsPromises.lstat(item.path);
+        let srcFileLstat = await fsPromises.lstat(filePath);
         if (srcFileLstat.isSymbolicLink()) {
-          await createSymlink(directory, item.cwd, item.base, item.path);
+          await createSymlink(linkPath, filePath);
         }
       }
       dirList.length = 0;
@@ -68,12 +66,14 @@ export default function gulpDestSymlink(directory) {
 
 // cwd, base, path 都是絕對路徑，cwd 不一定等於 process.cwd()
 // directory 可以是絕對或相對路徑，相對路徑是相對於 process.cwd() (和 gulp.dest() 相同)
-async function createSymlink(directory, cwd, base, filePath) {
-  let linkPath = path.join(
+function _resolveLinkPath(directory, cwd, base, filePath) {
+  return path.join(
     path.resolve(cwd, directory),
     path.relative(base, filePath),
   );
-  let linkDirPath = path.join(linkPath, '..');
+}
+async function createSymlink(linkPath, filePath) {
+  let linkDirPath = path.dirname(linkPath);
   let linkTarget = path.relative(linkDirPath, filePath);
   await fsMkdir(linkDirPath);
   await fsSymlink(linkTarget, linkPath);
