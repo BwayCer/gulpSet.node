74fb4f562fc0109c55972cc4a4c0bba1aa2d5d4c
BwayCer <bwaycer@gmail.com>
2023-07-13 18:37:17 +0800

    feat: modify 獨立 gulpTask 文件避免 utils 去載入 gulp

--- src/utils/tool.js
+++ src/utils/tool.js
@@ -2,8 +2,6 @@
 import path from 'path';
 import {Readable, Writable, Transform} from 'stream';
 
-import {gulp} from './gulpPkg.js';
-
 
 /**
  * 改變選取路徑：
@@ -95,68 +93,5 @@ export function insertPipe(buildTransform) {
     }))
   ;
   return transform;
-}
-
-
-/**
- * 創建任務：
- * 以彈性的物件對象取代函式方式來建立任務。
- *
- * @func gulpTask
- * @param {Object} info
- * @param {?String} info.name
- * @param {?String} info.srcBase
- * @param {(String|Array)} info.src
- * @param {?Object} info.srcOption
- * @param {Function} info.build
- * @param {?(string|function)} info.dest
- * @param {?Object} info.destOption
- * @param {?stream.Writable} info.writable
- * @return {Function}
- *
- * @example
- * function doTaskA() {
- *   return gulp.src(<globs>, <srcOption>)
- *     .pipe(insertPipe(<buildTransform>))
- *     .pipe(gulp.dest(<dest>, <destOption>))
- *   ;
- * }
- * // same as
- * gulpTask({
- *   name: 'doTaskA',
- *   src: <globs>,
- *   srcOption: <srcOption>,
- *   build: <buildTransform>,
- *   dest: <dest>,
- *   destOption: <destOption>,
- * });
- */
-// NOTE: 如果只用 `Transform` 則只會觸發最後一個 `pipe()`。
-export function gulpTask(info) {
-  ['src', 'build'].forEach(member => {
-    if (member in info) {
-      return;
-    }
-    throw new Error(`Missing required property "${member}" in the "info" object.`);
-  });
-  let fn = () => {
-    let globs = 'srcBase' in info
-      ? changeGlobs(info.srcBase, info.src)
-      : info.src
-    ;
-    let streamTask = gulp.src(globs, info.srcOption)
-      .pipe(insertPipe(info.build))
-    ;
-    if ('dest' in info) {
-      streamTask.pipe(gulp.dest(info.dest, info.destOption));
-    } else if ('writable' in info) {
-      streamTask.pipe(info.writable);
-    }
-    return streamTask;
-  };
-  if (typeof info.name === 'string') {
-    Reflect.defineProperty(fn, 'name', {value: info.name});
-  }
-  return fn;
 }
 
